a6cedf558e451d34dc66adb0d61bfb52f9350013
Fagner-Carvalho <fagner.m.carvalho@gmail.com>
2021-05-23 15:34:47 -0300

    Create Category refatorado com TSyringe

--- src/routes/categories.routes.ts
+++ src/routes/categories.routes.ts
@@ -1,7 +1,7 @@
 import { Router } from "express";
 import multer from "multer";
 
-import createCategoryController from "../modules/cars/useCases/createCategory";
+import { CreateCategoryController } from "../modules/cars/useCases/createCategory/CreateCategoryController";
 import { importCategoryController } from "../modules/cars/useCases/importCategory";
 import { listCategoriesController } from "../modules/cars/useCases/listCategories";
 
@@ -11,9 +11,9 @@ const upload = multer({
   dest: "./tmp",
 });
 
-categoriesRountes.post("/", (request, response) => {
-  return createCategoryController().handle(request, response);
-});
+const createCategoryController = new CreateCategoryController();
+
+categoriesRountes.post("/", createCategoryController.handle);
 
 categoriesRountes.get("/", (request, response) => {
   return listCategoriesController.handle(request, response);
